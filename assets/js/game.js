befb62bb3842b04615d68705d815950c9258a13a
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-02-07 15:52:56 -0500

    learn JavaScript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight() {
+    window.alert("The fight has begun!");
+}
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+
+
+// fight();
